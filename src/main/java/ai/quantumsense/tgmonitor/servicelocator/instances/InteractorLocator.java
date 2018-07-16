9464dbd41129c1fdc8b3b8b82c78bf831e5e80dd
Daniel Weibel <danielmweibel@gmail.com>
2018-07-16 16:36:15 +0800

    Remove log output from InteractorLocator

--- src/main/java/ai/quantumsense/tgmonitor/servicelocator/instances/InteractorLocator.java
+++ src/main/java/ai/quantumsense/tgmonitor/servicelocator/instances/InteractorLocator.java
@@ -9,13 +9,11 @@ public class InteractorLocator implements ServiceLocator<Interactor> {
 
     @Override
     public void registerService(Interactor interactor) {
-        System.out.println("InteractorLocator: registering interactor : " + interactor);
         instance = interactor;
     }
 
     @Override
     public Interactor getService() {
-        System.out.println("InteractorLocator: returning " + instance);
         if (instance == null)
             Util.failNotYetRegistered(this.getClass());
         return instance;
